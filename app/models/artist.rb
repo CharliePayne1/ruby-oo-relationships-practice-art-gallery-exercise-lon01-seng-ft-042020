4b0d4c4762c8e96143b8424d1ea891f4c735cf50
CharliePayne1 <charliepayne1101@gmail.com>
2020-04-25 18:46:44 +0100

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,40 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select {|painting| painting.artist == self}
+  end
+
+  def galleries
+    paintings.map {|painting| painting.gallery}.uniq
+  end
+
+  def cities
+    galleries.map {|gallery| gallery.city}
+  end
+
+  def self.total_experience
+    Artist.all.reduce(0) {|sum, artist| sum + artist.years_experience}
+  end
+
+  def self.most_prolific
+    Artist.all.max_by {|artist| artist.paintings.length / artist.years_experience}
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,22 @@
 require_relative '../config/environment.rb'
 
 
+charlie = Artist.new("Charlie", 1)
+will = Artist.new("William", 2)
+james = Artist.new("James", 3)
+matthew = Artist.new("Matthew", 4)
+jane = Artist.new("Jane", 5)
 
+london = Gallery.new("Tate", "London")
+paris = Gallery.new("Louvre", "Paris")
+london2 = Gallery.new("NHM", "London")
+newyork = Gallery.new("NY", "New York")
+
+painting1 = Painting.new("hello", 10, charlie, london)
+painting2 = Painting.new("hey", 20, will, paris)
+painting3 = Painting.new("hi", 30, james, london2)
+painting4 = Painting.new("howdy", 40, matthew, newyork)
+painting5 = Painting.new("hiya", 50, charlie, london)
 
 binding.pry
 
